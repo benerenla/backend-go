09dee190f2d0cca317c0ffaf4f77d308325f1070
Eren atlas <88831406+benerenla@users.noreply.github.com>
2022-05-30 12:37:06 +0300

    add test

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"github.com/Constani/main/routers"
 	"github.com/Constani/main/utils"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     "http://localhost:8080",
+		AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
+	}))
 	os.Setenv("LOG_FILE", "log.log")
 	app.Get("/", routers.GetMain)
 	utils.Connect()
---
+++ test/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express()
+const axios = require('axios')
+app.get("/", async (req, res) => {
+    const data = await axios.get("http://localhost:3000/api/v1/anime")
+    res.send(data.data)
+})
+
+app.listen(3001)
